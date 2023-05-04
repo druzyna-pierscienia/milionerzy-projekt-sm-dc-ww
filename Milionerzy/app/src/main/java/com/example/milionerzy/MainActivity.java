7b966c7d7e514c821a4e140dd0948bb59e0895cf
quizark <35684@student.pwsztar.edu.pl>
2023-05-04 09:23:22 +0200

    ButtonsV2.1
    Small update for buttons

--- Milionerzy/app/src/main/java/com/example/milionerzy/MainActivity.java
+++ Milionerzy/app/src/main/java/com/example/milionerzy/MainActivity.java
@@ -7,20 +7,34 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    private Button registerButton, loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button registerButton = (Button) findViewById(R.id.register_button);
-        registerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,RegisterActivity.class);
+        registerButton = findViewById(R.id.register_button);
+        loginButton = findViewById(R.id.login_button);
+
+        registerButton.setOnClickListener(this);
+        loginButton.setOnClickListener(this);
+    };
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.register_button:
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
                 startActivity(intent);
-            }
-        });
+                break;
+            case R.id.login_button:
+
+                //TODO:Warunek sprawdzający czy zalogowano
+
+                Intent intent1 = new Intent(MainActivity.this, MainButtons.class);
+                startActivity(intent1);
+        }
     }
 }
