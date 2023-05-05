8d8b6f19ca777d7d844eb0dc9d2343a824162b5c
quizark <35684@student.pwsztar.edu.pl>
2023-05-05 12:39:23 +0200

    Buttons Update

--- Milionerzy/app/src/main/java/com/example/milionerzy/GameButtons.java
+++ Milionerzy/app/src/main/java/com/example/milionerzy/GameButtons.java
@@ -2,6 +2,7 @@ package com.example.milionerzy;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,14 +12,21 @@ import androidx.appcompat.app.AppCompatActivity;
 public class GameButtons extends AppCompatActivity implements View.OnClickListener {
 
     public Integer score = 0, questionLvl = 1;
-    public String correctAnswer = "A";
+    //TODO: Cała poniższa linijka do zmiany na pobieranie z bazy
+    public String correctAnswer = "A", questionFromDataBase = "Pytanie z bazy pytań", answerAFromDataBase = "Odpowiedź A", answerBFromDataBase = "Odpowiedź B", answerCFromDataBase = "Odpowiedź C", answerDFromDataBase = "Odpowiedź D";
 
     private TextView question;
     private Button answerA, answerB, answerC, answerD, hint5050, hintAudience, hintPhone, backToMenu;
 
+
+
     protected void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
         setContentView(R.layout.question);
+
+        Intent intent = getIntent();
+        questionLvl = intent.getIntExtra("questionLvl", 1); // pobranie przekazanej wartości 'questionLvl'
+        score = intent.getIntExtra("score", 0); // pobranie przekazanej wartości 'score'
 
         question = findViewById(R.id.question_text);
         answerA = findViewById(R.id.odpA);
@@ -30,11 +38,11 @@ public class GameButtons extends AppCompatActivity implements View.OnClickListen
         hintPhone = findViewById(R.id.hintPhone);
         backToMenu = findViewById(R.id.backToMenuButton);
 
-        question.setText("PYTANIE ASD - test");
-        answerA.setText("ODPOWIEDŹ A - test");
-        answerB.setText("ODPOWIEDŹ B - test");
-        answerC.setText("ODPOWIEDŹ C - test");
-        answerD.setText("ODPOWIEDŹ D - test");
+        question.setText(questionFromDataBase + questionLvl);
+        answerA.setText(answerAFromDataBase);
+        answerB.setText(answerBFromDataBase);
+        answerC.setText(answerCFromDataBase);
+        answerD.setText(answerDFromDataBase);
 
         answerA.setOnClickListener(this);
         answerB.setOnClickListener(this);
@@ -44,54 +52,184 @@ public class GameButtons extends AppCompatActivity implements View.OnClickListen
         hintAudience.setOnClickListener(this);
         hintPhone.setOnClickListener(this);
         backToMenu.setOnClickListener(this);
+
+
     }
 
     @Override
     public void onClick(View v) {
+
+
+
         switch (v.getId()) {
             case R.id.odpA:
-                if (correctAnswer == "A") {
-                    score = score + (questionLvl * 10);
-                    questionLvl += 1;
-                    //TODO: Wywołanie gry jeszcze raz z nowymi pytaniami
-                } else {
-                    Intent intent = new Intent(GameButtons.this, AboutApp.class);
-                    intent.putExtra("score", score);
-                    startActivity(intent);
-                }
+                answerA.setBackgroundColor(getResources().getColor(R.color.orange));
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    public void run() {
+                        if (correctAnswer == "A") {
+                            answerA.setBackgroundColor(getResources().getColor(R.color.green));
+                            score = score + (questionLvl * 10);
+                            questionLvl += 1;
+
+                            Intent intent = new Intent(GameButtons.this, GameButtons.class);
+                            intent.putExtra("questionLvl", questionLvl);
+                            intent.putExtra("score", score);
+                            startActivity(intent);
+
+                            //TODO: Dodać warunek końca gry
+                        } else {
+                            answerA.setBackgroundColor(getResources().getColor(R.color.red));
+                            Intent intent = new Intent(GameButtons.this, ending_screen.class);
+                            intent.putExtra("score", score);
+                            intent.putExtra("question", questionFromDataBase);
+                            if(correctAnswer == "A") {
+                                String correctAnswerAway = answerAFromDataBase;
+                                intent.putExtra("correctAnswer", correctAnswerAway);
+                            }
+                            else if(correctAnswer == "B") {
+                                String correctAnswerAway = answerBFromDataBase;
+                                intent.putExtra("correctAnswer", correctAnswerAway);
+                            }
+                            else if(correctAnswer == "C") {
+                                String correctAnswerAway = answerCFromDataBase;
+                                intent.putExtra("correctAnswer", correctAnswerAway);
+                            }
+                            else {
+                                String correctAnswerAway = answerDFromDataBase;
+                                intent.putExtra("correctAnswer", correctAnswerAway);
+                            }
+
+                            startActivity(intent);
+                        }
+                    }
+                }, 1000);
                 break;
             case R.id.odpB:
-                if (correctAnswer == "B") {
-                    score = score + (questionLvl * 10);
-                    questionLvl += 1;
-                    //TODO: Wywołanie gry jeszcze raz z nowymi pytaniami
-                } else {
-                    Intent intent = new Intent(GameButtons.this, ending_screen.class);
-                    intent.putExtra("score", score);
-                    startActivity(intent);
-                }
+                answerB.setBackgroundColor(getResources().getColor(R.color.orange));
+                Handler handler1 = new Handler();
+                handler1.postDelayed(new Runnable() {
+                    public void run() {
+                        if (correctAnswer == "B") {
+                            answerB.setBackgroundColor(getResources().getColor(R.color.green));
+                            score = score + (questionLvl * 10);
+                            questionLvl += 1;
+
+                            Intent intent = new Intent(GameButtons.this, GameButtons.class);
+                            intent.putExtra("questionLvl", questionLvl);
+                            intent.putExtra("score", score);
+                            startActivity(intent);
+
+                            //TODO: Dodać warunek końca gry
+                        } else {
+                            answerB.setBackgroundColor(getResources().getColor(R.color.red));
+                            Intent intent = new Intent(GameButtons.this, ending_screen.class);
+                            intent.putExtra("score", score);
+                            intent.putExtra("question", questionFromDataBase);
+                            if(correctAnswer == "A") {
+                                String correctAnswerAway = answerAFromDataBase;
+                                intent.putExtra("correctAnswer", correctAnswerAway);
+                            }
+                            else if(correctAnswer == "B") {
+                                String correctAnswerAway = answerBFromDataBase;
+                                intent.putExtra("correctAnswer", correctAnswerAway);
+                            }
+                            else if(correctAnswer == "C") {
+                                String correctAnswerAway = answerCFromDataBase;
+                                intent.putExtra("correctAnswer", correctAnswerAway);
+                            }
+                            else {
+                                String correctAnswerAway = answerDFromDataBase;
+                                intent.putExtra("correctAnswer", correctAnswerAway);
+                            }
+                            startActivity(intent);
+                        }
+                    }
+                }, 1000);
                 break;
             case R.id.odpC:
-                if (correctAnswer == "C") {
-                    score = score + (questionLvl * 10);
-                    questionLvl += 1;
-                    //TODO: Wywołanie gry jeszcze raz z nowymi pytaniami
-                } else {
-                    Intent intent = new Intent(GameButtons.this, ending_screen.class);
-                    intent.putExtra("score", score);
-                    startActivity(intent);
-                }
+                answerC.setBackgroundColor(getResources().getColor(R.color.orange));
+                Handler handler2 = new Handler();
+                handler2.postDelayed(new Runnable() {
+                    public void run() {
+                        if (correctAnswer == "C") {
+                            answerC.setBackgroundColor(getResources().getColor(R.color.green));
+                            score = score + (questionLvl * 10);
+                            questionLvl += 1;
+
+                            Intent intent = new Intent(GameButtons.this, GameButtons.class);
+                            intent.putExtra("questionLvl", questionLvl);
+                            intent.putExtra("score", score);
+                            startActivity(intent);
+
+                            //TODO: Dodać warunek końca gry
+                        } else {
+                            answerC.setBackgroundColor(getResources().getColor(R.color.red));
+                            Intent intent = new Intent(GameButtons.this, ending_screen.class);
+                            intent.putExtra("score", score);
+                            intent.putExtra("question", questionFromDataBase);
+                            if(correctAnswer == "A") {
+                                String correctAnswerAway = answerAFromDataBase;
+                                intent.putExtra("correctAnswer", correctAnswerAway);
+                            }
+                            else if(correctAnswer == "B") {
+                                String correctAnswerAway = answerBFromDataBase;
+                                intent.putExtra("correctAnswer", correctAnswerAway);
+                            }
+                            else if(correctAnswer == "C") {
+                                String correctAnswerAway = answerCFromDataBase;
+                                intent.putExtra("correctAnswer", correctAnswerAway);
+                            }
+                            else {
+                                String correctAnswerAway = answerDFromDataBase;
+                                intent.putExtra("correctAnswer", correctAnswerAway);
+                            }
+                            startActivity(intent);
+                        }
+                    }
+                }, 1000);
                 break;
             case R.id.odpD:
-                if (correctAnswer == "D") {
-                    score = score + (questionLvl * 10);
-                    questionLvl += 1;
-                    //TODO: Wywołanie gry jeszcze raz z nowymi pytaniami
-                } else {
-                    Intent intent = new Intent(GameButtons.this, ending_screen.class);
-                    intent.putExtra("score", score);
-                    startActivity(intent);
-                }
+                answerD.setBackgroundColor(getResources().getColor(R.color.orange));
+                Handler handler3 = new Handler();
+                handler3.postDelayed(new Runnable() {
+                    public void run() {
+                        if (correctAnswer == "D") {
+                            answerD.setBackgroundColor(getResources().getColor(R.color.green));
+                            score = score + (questionLvl * 10);
+                            questionLvl += 1;
+
+                            Intent intent = new Intent(GameButtons.this, GameButtons.class);
+                            intent.putExtra("questionLvl", questionLvl);
+                            intent.putExtra("score", score);
+                            startActivity(intent);
+
+                            //TODO: Dodać warunek końca gry
+                        } else {
+                            answerD.setBackgroundColor(getResources().getColor(R.color.red));
+                            Intent intent = new Intent(GameButtons.this, ending_screen.class);
+                            intent.putExtra("score", score);
+                            intent.putExtra("question", questionFromDataBase);
+                            if(correctAnswer == "A") {
+                                String correctAnswerAway = answerAFromDataBase;
+                                intent.putExtra("correctAnswer", correctAnswerAway);
+                            }
+                            else if(correctAnswer == "B") {
+                                String correctAnswerAway = answerBFromDataBase;
+                                intent.putExtra("correctAnswer", correctAnswerAway);
+                            }
+                            else if(correctAnswer == "C") {
+                                String correctAnswerAway = answerCFromDataBase;
+                                intent.putExtra("correctAnswer", correctAnswerAway);
+                            }
+                            else {
+                                String correctAnswerAway = answerDFromDataBase;
+                                intent.putExtra("correctAnswer", correctAnswerAway);
+                            }
+                            startActivity(intent);
+                        }
+                    }
+                }, 1000);
                 break;
             case R.id.hint5050:
                 // TODO: Koło ratunkowe - 50/50
--- Milionerzy/app/src/main/java/com/example/milionerzy/ending_screen.java
+++ Milionerzy/app/src/main/java/com/example/milionerzy/ending_screen.java
@@ -15,9 +15,17 @@ public class ending_screen extends AppCompatActivity {
 
         Intent intent = getIntent();
         int score = intent.getIntExtra("score", 0); // pobranie przekazanej wartości 'score'
+        String question = intent.getStringExtra("question"); // pobranie przekazanego tekstu pytania
+        String correctAnswer = intent.getStringExtra("correctAnswer"); // pobranie przekazanej poprawnej odpowiedzi
+
 
         TextView scoreTextView = findViewById(R.id.final_score);
+        TextView questionTV = findViewById(R.id.question);
+        TextView correctAnswerTV = findViewById(R.id.correct_answer);
+
         scoreTextView.setText("Twój wynik: " + score);
+        questionTV.setText("Pytanie: " + question);
+        correctAnswerTV.setText("Poprawna odpowiedź: " + correctAnswer);
 
         Button backButton = (Button) findViewById(R.id.back_button);
         backButton.setOnClickListener(new View.OnClickListener() {
