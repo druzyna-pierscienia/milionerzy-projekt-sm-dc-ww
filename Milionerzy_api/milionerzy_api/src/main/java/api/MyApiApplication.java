fce00bf2bb60b7d52aa5d6ca61a81152de57565c
CiuraD <127660370+CiuraD@users.noreply.github.com>
2023-05-07 11:30:55 +0200

    commiting api
    this appi finally works locally, plus it returns question with answers

---
+++ Milionerzy_api/milionerzy_api/src/main/java/api/MyApiApplication.java
@@ -0,0 +1,65 @@
+package api;
+
+import connection.Connect;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Random;
+
+@SpringBootApplication
+@RestController
+public class MyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyApiApplication.class, args);
+    }
+
+    @GetMapping("/question")
+    public String getQuestion(@RequestParam(name = "roundNumber") String roundNumber) {
+        Connect connect = new Connect();
+        Connection connection = connect.getConnection();
+        String pytanie = "blad";
+        if (connection != null) {
+            try {
+                // Tworzenie zapytania SQL
+                String query = "SELECT * FROM milionerzy.pytania WHERE numer_rundy = "+roundNumber;
+                Random random = new Random();
+                int randomNumber = random.nextInt(9);
+                // Wykonanie zapytania
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery(query);
+
+                // Przetwarzanie wyników zapytania
+                for(int i = 0; i < randomNumber; i++) {
+                    resultSet.next();
+                }
+                // Pobieranie wartości z kolumn w wyniku zapytania
+
+                pytanie = resultSet.getString("tresc");
+                pytanie += "/"+resultSet.getString("odpowiedz_a");
+                pytanie += "/"+resultSet.getString("odpowiedz_b");
+                pytanie += "/"+resultSet.getString("odpowiedz_c");
+                pytanie += "/"+resultSet.getString("odpowiedz_d");
+                pytanie += "/"+resultSet.getString("prawidlowa");
+
+                // Zamknięcie obiektów ResultSet i Statement
+                resultSet.close();
+                statement.close();
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } finally {
+                connect.close(); // Zamknięcie połączenia
+            }
+
+        }
+        return pytanie;
+    }
+}
