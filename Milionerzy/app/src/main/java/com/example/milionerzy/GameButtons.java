a20e9c6082601a4f03989e7337f573c6c1e56d2b
quizark <35684@student.pwsztar.edu.pl>
2023-06-11 18:03:12 +0200

    Question
    HotFix

--- Milionerzy/app/src/main/java/com/example/milionerzy/GameButtons.java
+++ Milionerzy/app/src/main/java/com/example/milionerzy/GameButtons.java
@@ -14,6 +14,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.ExecutionException;
 
